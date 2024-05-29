1f9cc4a4f521c58d13a1db25bd1f5ac07d2bbbca
Hyunjin-Jeong <currenjin@naver.com>
2024-05-29 16:16:07 +0900

    Time: 91 ms (46.29%), Space: 12.6 MB (19.22%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -4,15 +4,13 @@ class Solution(object):
         right = len(numbers) - 1
         
         while(left < right):
-            total = numbers[left] + numbers[right]
-            
-            if(total == target):
+            if(numbers[left] + numbers[right] == target):
                 return [left + 1, right + 1]
             
-            if(total > target):
+            if(numbers[left] + numbers[right] > target):
                 right -= 1
                 continue
                 
-            if(total < target):
+            if(numbers[left] + numbers[right] < target):
                 left += 1
                 continue
