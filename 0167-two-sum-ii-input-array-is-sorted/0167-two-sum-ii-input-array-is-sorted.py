84522ec4f85507fa9688a732ca9ebce4ca7ad7a1
Hyunjin-Jeong <currenjin@naver.com>
2024-05-29 16:19:39 +0900

    Time: 82 ms (91.39%), Space: 12.6 MB (42.57%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def twoSum(self, numbers, target):
-        left = 0
-        right = len(numbers) - 1
+        left, right = 0, len(numbers) - 1
         
         while(left < right):
             total = numbers[left] + numbers[right]
