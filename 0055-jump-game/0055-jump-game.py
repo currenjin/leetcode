2d05c212f93da68a8ab638d2445c907c4c0f6dd6
Hyunjin-Jeong <currenjin@naver.com>
2024-06-18 13:27:43 +0900

    Time: 378 ms (20.21%), Space: 12.6 MB (70.12%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -2,7 +2,6 @@ class Solution(object):
     def canJump(self, nums):
         index = 0
         for i in nums:
-            if index < 0:
-                return False
+            if index < 0: return False
             index = max(i, index) - 1
         return True
