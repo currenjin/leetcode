c55083f2a1c7d1dd25360e5e040f64f2c6ca0b58
Hyunjin-Jeong <currenjin@naver.com>
2024-06-18 13:23:55 +0900

    Time: 476 ms (14.32%), Space: 12.4 MB (99.12%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def canJump(self, nums):
+        index = 0
+        for i in nums:
+            if index < 0:
+                return False
+            index = max(i, index) - 1
+        return True
