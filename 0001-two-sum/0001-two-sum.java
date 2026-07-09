e947ff15f601a5c60a96bead6c269daf057f688a
Daniel Jeong <hyun0524e@naver.com>
2026-07-09 13:09:03 +0900

    Time: 45 ms (25.59%), Space: 46.9 MB (83.93%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,12 +5,11 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (target == nums[i] + nums[j]) {
-                                results.add(i);
-                                results.add(j);
+                                return new int[] {i, j};
                 }
             }
         }
 
-        return results.stream().mapToInt(Integer::intValue).toArray();
+        return new int[] {0};
     }
 }
