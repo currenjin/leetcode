2828e42b57d54e3f7b7ad4aca99069c2ba7069f1
정현진(Daniel Jeong) <hyun0524e@naver.com>
2026-08-04 12:29:07 +0900

    Time: 208 ms (93.15%), Space: 27.6 MB (92.97%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def numIslands(self, grid):
+        land = 0
+        queue = deque()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        rows = len(grid)
+        cols = len(grid[0])
+
+        for r, row in enumerate(grid):
+            for c, col in enumerate(row):
+                if col == "1":
+                    land += 1
+                    queue.append((r, c))
+                    grid[r][c] = "0"
+
+                    while queue:
+                        curr_r, curr_c = queue.popleft()
+
+                        for dr, dc in directions:
+                            nr = curr_r + dr
+                            nc = curr_c + dc
+
+                            if not (0 <= nr < rows and 0 <= nc < cols): continue
+                            if grid[nr][nc] == "0": continue
+
+                            grid[nr][nc] = "0"
+                            queue.append((nr, nc))
+
+        
+
+        return land
