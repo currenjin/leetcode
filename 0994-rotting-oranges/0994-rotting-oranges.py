8ea16e7b8c969c20693c434d07ae30d6a9742bc7
정현진(Daniel Jeong) <hyun0524e@naver.com>
2026-08-09 17:45:18 +0900

    Time: 4 ms (72.25%), Space: 12.3 MB (59.53%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,44 +1,34 @@
 class Solution(object):
     def orangesRotting(self, grid):
-        minutes = 0
         fresh_count = 0
+        minutes = 0
         queue = deque()
         
         for r, row in enumerate(grid):
             for c, col in enumerate(row):
-                if col == 2:
-                    queue.append((r, c))
-                elif col == 1:
-                    fresh_count += 1
-        
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                if col == 2: queue.append((r, c))
+                if col == 1: fresh_count += 1
+
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         rows = len(grid)
         cols = len(grid[0])
-
-        while queue:
-            current_count = len(queue)
-
-            for _ in range(current_count):
+        while queue and fresh_count:
+            for _ in range(len(queue)):
                 r, c = queue.popleft()
-            
+
                 for dr, dc in directions:
-                    nr = r + dr
-                    nc = c + dc
+                    nr = dr + r
+                    nc = dc + c
 
-                    if not (0 <= nr < rows and 0 <= nc < cols):
-                        continue
-                    
-                    if grid[nr][nc] != 1:
-                        continue
-                    
+                    if not (0 <= nr < rows and 0 <= nc < cols): continue
+                    if grid[nr][nc] != 1: continue
+
                     grid[nr][nc] = 2
-                    fresh_count -= 1
                     queue.append((nr, nc))
+                    fresh_count -= 1
+            minutes += 1
 
-            if queue:
-                minutes += 1
-
-        if fresh_count == 0:
-            return minutes
-
+        if fresh_count == 0: return minutes
         return -1
+        
+
