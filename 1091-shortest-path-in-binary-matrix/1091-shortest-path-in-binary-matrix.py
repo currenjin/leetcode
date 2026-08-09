c97c11f30333a145babdb41884b7a162bff928f1
정현진(Daniel Jeong) <hyun0524e@naver.com>
2026-08-09 18:01:50 +0900

    Time: 162 ms (98.98%), Space: 12.5 MB (97.95%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -2,32 +2,29 @@ class Solution(object):
     def shortestPathBinaryMatrix(self, grid):
         if grid[0][0] == 1 or grid[-1][-1] == 1: return -1
 
-        distance = 1
+        count = 1
         queue = deque()
-        queue.append((0, 0))
-        grid[0][0] = 1
 
+        queue.append((0, 0))
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
         rows = len(grid)
         cols = len(grid[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-
         while queue:
             for _ in range(len(queue)):
-                curr_r, curr_c = queue.popleft()
-                if curr_r == rows - 1 and curr_c == cols -1: return distance
+                cr, cc = queue.popleft()
+                if (cr == rows - 1 and cc == cols - 1): return count
 
                 for dr, dc in directions:
-                    nr = curr_r + dr
-                    nc = curr_c + dc
+                    nr = dr + cr
+                    nc = dc + cc
 
                     if not (0 <= nr < rows and 0 <= nc < cols): continue
                     if grid[nr][nc] == 1: continue
 
-                    grid[nr][nc] = 1
                     queue.append((nr, nc))
+                    grid[nr][nc] = 1
                 
-            distance += 1
+            count += 1
+
 
         return -1
-                
-        
