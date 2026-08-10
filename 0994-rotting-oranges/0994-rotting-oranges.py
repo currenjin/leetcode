4babc0ae1ae3717db791cce1a371f5903e4b0f5b
정현진(Daniel Jeong) <hyun0524e@naver.com>
2026-08-10 18:11:21 +0900

    Time: 0 ms (100%), Space: 12.4 MB (22.84%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,34 +1,32 @@
 class Solution(object):
     def orangesRotting(self, grid):
-        fresh_count = 0
         minutes = 0
+        fresh_count = 0
         queue = deque()
-        
+
         for r, row in enumerate(grid):
             for c, col in enumerate(row):
                 if col == 2: queue.append((r, c))
                 if col == 1: fresh_count += 1
-
+        
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         rows = len(grid)
         cols = len(grid[0])
-        while queue and fresh_count:
+        while queue and fresh_count != 0:
             for _ in range(len(queue)):
-                r, c = queue.popleft()
-
+                cr, cc = queue.popleft()
                 for dr, dc in directions:
-                    nr = dr + r
-                    nc = dc + c
+                    nr = dr + cr
+                    nc = dc + cc
 
                     if not (0 <= nr < rows and 0 <= nc < cols): continue
                     if grid[nr][nc] != 1: continue
 
-                    grid[nr][nc] = 2
                     queue.append((nr, nc))
+                    grid[nr][nc] = 2
                     fresh_count -= 1
+
             minutes += 1
 
         if fresh_count == 0: return minutes
         return -1
-        
-
