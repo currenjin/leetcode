73dfc55e14c5dd040a9bf9832853d20d34c205bd
정현진(Daniel Jeong) <hyun0524e@naver.com>
2026-08-11 11:20:23 +0900

    Time: 178 ms (57.87%), Space: 15.7 MB (58.87%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def updateMatrix(self, mat):
+        queue = deque()
+            
+        rows = len(mat)
+        cols = len(mat[0])
+        
+        distance = [[-1] * cols for _ in range(rows)]
+
+        for r, row in enumerate(mat):
+            for c, col in enumerate(row):
+                if col == 0:
+                    queue.append((r, c))
+                    distance[r][c] = 0
+
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        while queue:
+            for _ in range(len(queue)):
+                cr, cc = queue.popleft()
+                
+                for dr, dc in directions:
+                    nr = dr + cr
+                    nc = dc + cc
+
+                    if not (0 <= nr < rows and 0 <= nc < cols): continue
+                    if distance[nr][nc] != -1: continue
+
+                    distance[nr][nc] = distance[cr][cc] + 1
+                    queue.append((nr, nc))
+
+        return distance
+
+
+
+
+
+
+                
