17882bd8f287f94786caef52add3640b87985d70
정현진(Daniel Jeong) <hyun0524e@naver.com>
2026-08-12 16:40:55 +0900

    Time: 74 ms (93.7%), Space: 13.6 MB (86.88%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -1,22 +1,28 @@
 class Solution(object):
     def nearestExit(self, maze, entrance):
         """
+        Before
         1. 새로운 배열을 구현한다. (벽은 방문 처리, 출구를 queue에 담는다.)
         2. BFS를 돌린다. (다음 위치가 출구가 있는 곳이라면 완료가 되어야 함)
         3. 출구를 못 찾으면 -1
+
+        시간복잡도: O(rows * cols)
+        공간복잡도: O(rows * cols)
+
+        After
+        1. 입구를 큐에 저장한다.
+        2. BFS를 돌린다.
+            방문은 + 처리를 한다.
+            r이 0 or rows - 1, c가 0 or cols - 1이면 완료다.
+        3. 출구를 못 찾으면 -1
         """
+
+        queue = deque()
+        queue.append((entrance[0], entrance[1]))
+        maze[entrance[0]][entrance[1]] = "+"
 
         rows = len(maze)
         cols = len(maze[0])
-
-        queue = deque()
-        copy_maze = [[0] * cols for _ in range(rows)]
-
-        for r, row in enumerate(maze):
-            for c, col in enumerate(row):
-                if col == "+": copy_maze[r][c] = 1
-                if [r, c] == entrance: continue
-                if col == "." and (r == 0 or c == 0 or r == rows - 1 or c == cols - 1): queue.append((r, c))
 
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         distance = 0
@@ -30,11 +36,10 @@ class Solution(object):
                     nc = dc + cc
 
                     if not (0 <= nr < rows and 0 <= nc < cols): continue
-                    if copy_maze[nr][nc] == 1: continue
-                    if nr == entrance[0] and nc == entrance[1]: return distance
-                    
+                    if maze[nr][nc] == "+": continue
+                    if nr == 0 or nr == rows - 1 or nc == 0 or nc == cols - 1: return distance
+
                     queue.append((nr, nc))
-                    copy_maze[nr][nc] = 1
-
-
+                    maze[nr][nc] = "+"
+        
         return -1
