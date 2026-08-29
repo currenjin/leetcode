3e4685f5d66c2cd6a5bdf136bcb61711f9df2459
정현진(Daniel Jeong) <hyun0524e@naver.com>
2026-08-29 23:18:52 +0900

    Time: 4 ms (28.86%), Space: 14.3 MB (32.52%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -1,11 +1,6 @@
 class Solution(object):
     def hasPathSum(self, root, targetSum):
         if root is None: return False
+        if targetSum - root.val == 0 and root.left is None and root.right is None: return True
 
-        result = targetSum - root.val
-        if root.left is None and root.right is None and result == 0: return True
-        
-        left = self.hasPathSum(root.left, result)
-        right = self.hasPathSum(root.right, result)
-
-        return left or right
+        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
