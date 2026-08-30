18abd6a697afe59d4369e0613592c2bb83c68228
정현진(Daniel Jeong) <hyun0524e@naver.com>
2026-08-30 19:00:06 +0900

    Time: 11 ms (20.32%), Space: 18 MB (41.43%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def isBalanced(self, root):
-        def calHeight(root):
-            if root is None: return 0
+        
+        def height(node):
+            if node is None: return 0
 
-            left = calHeight(root.left)
-            right = calHeight(root.right)
+            left = height(node.left)
+            right = height(node.right)
 
-            if left == -1 or right == -1: return -1
             if abs(left - right) > 1: return -1
-
+            if left == -1 or right == -1: return -1
             return max(left, right) + 1
 
-        return calHeight(root) != -1
+        return height(root) != -1
