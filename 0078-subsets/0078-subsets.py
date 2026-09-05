97fb93baf70f456e6a152bf8421a017ee488ac80
정현진(Daniel Jeong) <hyun0524e@naver.com>
2026-09-05 20:36:26 +0900

    Time: 0 ms (100%), Space: 12.4 MB (98.61%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,18 @@
 class Solution(object):
     def subsets(self, nums):
         results = []
-
+        
         def dfs(index, path):
             if index == len(nums):
                 results.append(path[:])
                 return
 
             dfs(index + 1, path)
-            
+
             path.append(nums[index])
             dfs(index + 1, path)
 
             path.pop()
-
+        
         dfs(0, [])
         return results
